c0478c0197f957330172a7741b12d1490ce5cc28
Linaeformin <minsa030407@gmail.com>
2025-05-30 00:37:55 +0900

    [FEAT] 사용자 데이터 베이스 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    email = models.EmailField(unique = True)
+    name = models.CharField(max_length = 100)
+    img_url = models.CharField(max_length = 255, null = True, blank = True)
+
+    def __str__(self):
+        return self.name
