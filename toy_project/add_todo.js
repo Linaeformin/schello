332126ebd43e129800b2df49cd91f6c7a594bc87
SongJi-eun6 <ssjieun32@gmail.com>
2025-06-03 21:02:57 +0900

    feat: '할 일 추가 페이지','프로필 페이지' 구현

---
+++ toy_project/add_todo.js
@@ -0,0 +1,34 @@
+const todoForm = document.getElementById('todoForm');
+
+        todoForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            // 값 읽어와서 변수에 저장
+            const todo = document.getElementById('todo').value;
+            const memo = document.getElementById('memo').value;
+            const year = document.getElementById('year').value;
+            const month = document.getElementById('month').value;
+            const day = document.getElementById('day').value;
+
+            const startHour = document.getElementById('start-hour').value;
+            const startMinute = document.getElementById('start-minute').value;
+            const endHour = document.getElementById('end-hour').value;
+            const endMinute = document.getElementById('end-minute').value;
+
+            const priorityRadio = document.querySelector('input[name="priority-radio"]:checked');
+            const priority = priorityRadio ? priorityRadio.value : null;
+
+            //값이 들어있는 todoData 객체
+            const todoData = {
+                todo,
+                memo,
+                date: `${year}-${month}-${day}`,
+                startTime: `${startHour}:${startMinute}`,
+                endTime: `${endHour}:${endMinute}`,
+                priority
+            };
+
+            localStorage.setItem('todoData', JSON.stringify(todoData));
+            //<- 로컬스토리지 저장 
+
+        });
