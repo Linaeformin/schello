25d84608f26b4262b01ed1239f242cb2289f9982
yb2527 <yb2527@swu.ac.kr>
2025-06-05 14:02:43 +0900

    일정 요소 수정
    # Conflicts:
    #	schedules/admin.py
    #	schedules/models.py
    #	schedules/views.py

--- schedules/models.py
+++ schedules/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from accounts.models import Member
-from django.utils import timezone
+# from accounts.models import Member
+from django.conf import settings
 
 class Schedule(models.Model):
     PRIORITY_CHOICES = [
@@ -9,14 +9,14 @@ class Schedule(models.Model):
         (3, '낮음'),
     ]
 
-    member = models.ForeignKey(Member, related_name = 'schedules', on_delete = models.CASCADE)
+    # member = models.ForeignKey(Member, related_name = 'schedules', on_delete = models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'schedules', on_delete = models.CASCADE)
     title = models.CharField(max_length = 20)  # 공백 포함 20자 제한
-    date = models.DateField(default=timezone.now) # 현재 날짜 자동 설정
+    date = models.DateField()
     start = models.TimeField(blank = True, null = True)
     end = models.TimeField(blank = True, null = True)
     memo = models.TextField(max_length = 200, blank = True)
     priority = models.IntegerField(choices = PRIORITY_CHOICES, null = True, blank = True)
 
     def __str__(self):
-        return f"{self.title} - {self.date}"
-
+        return f"{self.title} - {self.date} {self.start or ''} ~ {self.end or ''}"
