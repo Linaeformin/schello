83d34b6372bb772a950253a87e255ae0adbbb3e1
Linaeformin <minsa030407@gmail.com>
2025-06-07 02:21:14 +0900

    [FEAT] BE_FE_프로필 수정 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('logout-test/', logout_test_view, name='logout_test'),
     path('profile/', views.profile_view, name='profile'),
+    path('update/', views.update_profile_image, name='update_profile_image'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.decorators import login_required
 from .models import Member
+from django.views.decorators.http import require_POST
+import os
 import logging
 logger = logging.getLogger(__name__)
 
@@ -53,3 +55,27 @@ def profile_view(request):
         'img_url': member.img_url or '/static/accounts/default-profile.png',
     }
     return render(request, 'accounts/profile.html', context)
+
+@require_POST
+@login_required
+def update_profile_image(request):
+    member = Member.objects.get(user=request.user)
+    image = request.FILES.get('profile_image')
+
+    if image:
+        filename = image.name
+        save_path = os.path.join(settings.MEDIA_ROOT, 'profile_images', filename)
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        with open(save_path, 'wb+') as f:
+            for chunk in image.chunks():
+                f.write(chunk)
+
+        member.img_url = f'/media/profile_images/{filename}'
+        member.save()
+
+        # 🔥 여기 로그 찍기
+        logger.debug(f"[프로필 업데이트] 저장된 이미지 경로: {member.img_url}")
+        logger.debug(f"[프로필 업데이트] 저장 위치: {save_path}")
+
+    return redirect('profile')
