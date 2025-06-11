9d8052174b4d2af27886c791380cebbc9770ac82
yb2527 <yb2527@swu.ac.kr>
2025-06-11 15:32:29 +0900

    feat: 일정 추가

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'schedules'
+
 urlpatterns = [
     path('api/schedules/', views.get_schedule_api, name='get_schedule_api'),
     path('<int:pk>/update_checked_status/', views.schedule_update_checked_status,
          name='schedule_update_checked_status'),
+    path('add/', views.add_schedule_view, name='add_schedule'),
 ]
